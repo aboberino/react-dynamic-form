a7d9031f2502283ffb472eaf2ba7179cf0595158
Aimeric Boban <aimeric.boban@alithya.com>
2022-10-06 17:28:49 +0200

    Add new opts linked to useForm ⚙

--- src/components/ModalOptions/ModalOptions.tsx
+++ src/components/ModalOptions/ModalOptions.tsx
@@ -1,7 +1,7 @@
+import React from 'react'
 import { ActionIcon, Button, Group, Input, Modal, ScrollArea, Stack } from '@mantine/core'
-import { GetInputProps, GetInputPropsType, LooseKeys, UseFormReturnType } from '@mantine/form/lib/types'
+import { UseFormReturnType } from '@mantine/form/lib/types'
 import { IconPlaylistAdd, IconTrash } from '@tabler/icons'
-import React, { useState } from 'react'
 import { BlocParams } from '../BlocComponent'
 
 type Props = {
@@ -11,9 +11,26 @@ type Props = {
     index: number
 }
 
-export default function ModalOptions({ opened, setOpened, form, index }: Props) {
-    console.log('form', form)
+function getRandomNumber() {
+    return Math.floor(Math.random() * 1000000)
+}
 
+export default function ModalOptions({ opened, setOpened, form, index }: Props) {
+
+    function handleNewOption() {
+        const newOption = {
+            id: getRandomNumber(),
+            value: 'Default',
+            label: 'Default',
+            blocInputId: form.values.inputs[index]?.id,
+        }
+        form.insertListItem(`inputs.${index}.options`, newOption)
+    }
+
+    function handleDeleteOption(optIndex: number) {
+        console.log(optIndex, form.values.inputs[index]?.options)
+        form.removeListItem(`inputs.${index}.options`, optIndex)
+    }
 
     return (
         <Modal
@@ -24,21 +41,21 @@ export default function ModalOptions({ opened, setOpened, form, index }: Props) 
         >
             <ScrollArea style={{ height: 250, marginBottom: 12 }} >
                 <Stack>
-                    {form.values.inputs[index]?.options.map((input, i) => (<InputClearable key={i} form={form.getInputProps(`inputs.${index}.options.${i}.value`)} />))}
+                    {form.values.inputs[index]?.options.map((input, i) => (<InputClearable key={i} index={i} handleDeleteOption={handleDeleteOption} form={form.getInputProps(`inputs.${index}.options.${i}.value`)} />))}
                 </Stack>
             </ScrollArea>
             <Group position='right' spacing='xs'>
-                <Button leftIcon={<IconPlaylistAdd />} variant='outline'>Ajouter</Button>
+                <Button leftIcon={<IconPlaylistAdd />} variant='outline' onClick={handleNewOption}>Ajouter</Button>
                 <Button variant='gradient'>Enregistrer</Button>
             </Group>
         </Modal>
     )
 }
 
-function InputClearable({ form }: { form: any }) {
+function InputClearable({ index, handleDeleteOption, form }: { index: number, handleDeleteOption: (index: number) => void, form: any }) {
     return (
         <Group sx={{ gap: 4 }}>
-            <ActionIcon variant='light' color='red' size='lg'><IconTrash size={14} /></ActionIcon>
+            <ActionIcon variant='light' color='red' size='lg' onClick={() => handleDeleteOption(index)}><IconTrash size={14} /></ActionIcon>
             <Input variant="filled" placeholder="Label de l'input" {...form} />
         </Group>
     )
